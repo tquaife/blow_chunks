c1f800c83d3a3c85f2d07e45a50b62ac5d874c99
db903833 <t.l.quaife@reading.ac.uk>
2025-03-29 10:57:33 +0000

    re-written command line parser and usage function

--- blow_chunks.c
+++ blow_chunks.c
@@ -10,18 +10,18 @@ int	main( int argc, char **argv )
 	long int		i=0, num_samples;
 	long int		nlines=0, nwaves=0;
 	int             verbose=FALSE ;
-	float			*sample_value, duration=1.0;
+	float			*sample_value; 
+	float           default_duration=1.0;
 	
 	RIFF_hdr		wav_header;
 	chunk_hdr		fmt_header, data_header;
-	PCM_fmt_chnk		fmt_chunk;
+	PCM_fmt_chnk    fmt_chunk;
 	
 	struct	wave_node	*top_node;
     struct variable_node *var_node ;
     struct control ctrl; //not a pointer!
 	
-	void 			parser(  );
-	
+	void 			clparser(  );	
 	
 	/* -- Setup defaults -- */
 	fmt_chunk.Channels=1;
@@ -29,9 +29,11 @@ int	main( int argc, char **argv )
 	fmt_chunk.AvgBytesPerSec=88200;
 	fmt_chunk.BlockAlign=2;
 	fmt_chunk.PCM_bps=16;
+    ctrl.seq_duration=default_duration;
+    ctrl.total_length=default_duration;
 	
 	/*parse command line*/
-	parser( argc, argv, &duration, &fmt_chunk );
+	clparser( argc, argv, &ctrl, &fmt_chunk );
 
     /*set up in-built variables*/
     var_node = NULL ;
@@ -40,8 +42,8 @@ int	main( int argc, char **argv )
     /*set up the control structure*/
     ctrl.master_volume=1.0;
     ctrl.seq_start=0.0;
-    ctrl.seq_duration=duration;
-    ctrl.total_length=duration;
+    //ctrl.seq_duration=duration;
+    //ctrl.total_length=duration;
 
 	/* Set up the data structures */	
 	if ( ( top_node = setup_waveform_data_structures( &nlines, &nwaves, &fmt_chunk, var_node, &ctrl ) ) == NULL ){
@@ -78,7 +80,7 @@ int	main( int argc, char **argv )
 	/* Calculate and write data values */		
 	num_samples = ctrl.total_length * fmt_chunk.SampleRate ; 
 	
-	
+	/*loop over time (samples) and write out*/
 	while( i < num_samples ){		
 		calculate_data_value( top_node, &fmt_chunk, i, nwaves, sample_value );
 		write_pcm_data_sample( &fmt_chunk, sample_value );
@@ -90,60 +92,50 @@ int	main( int argc, char **argv )
 }
 
 
-void    parser( argc, argv, seconds, format )
+void    clparser( argc, argv, ctrl, format )
 int     argc;
 char    **argv;
-float   *seconds;
+struct control *ctrl ;
 PCM_fmt_chnk *format;
-        {
-        int     i = 1;
-        void    usage( );
+{
+    int     i = 1;
+    void    usage( );
 
-/*
-**      Derive options specified on the command-line.
-*/
-        for( i = 1; i < argc; i++ )
-        {       if( *argv[ i ] == '-' )
-                {       switch( *( argv[ i ] + 1 ) )
-						{      
-				case 'd':
-                                        *seconds = atof( argv[ ++i ] );
-					break; 
-										
-				/*	As of version 6 the number of channels will
-				be determined by the format of the input file */
-				
-				/*	
-                                case 'c':
-                                	format->Channels = ( short )atoi( argv[ ++i ] );
-                                	break;
-				*/
-                                
-				case 'b':
-                                        format->PCM_bps = ( short )atoi( argv[ ++i ] );
-                                        break;
-				case 'f':
-                                        format->SampleRate = ( long )atoi( argv[ ++i ] );
-                                        break;
-                                case 'u':
-                                        usage( argv[ 0 ] );
-                                        break;
-                                default:
-                                        usage( argv[ 0 ] );
-                                        break;                                  
-                        }
-		}
-	}
-	
+    for( i=1; i<argc; i++ ){
+
+        if( *argv[ i ] == '-' ){
+                
+            /**/ if( !strncasecmp(argv[ i ],"--duration",5) || !strcasecmp(argv[ i ],"-d") )   
+            {       
+                     ctrl->seq_duration = (float)atof( argv[ ++i ] );
+                     ctrl->total_length = ctrl->seq_duration ;
+            }
+            else if( !strncasecmp( argv[ i ],"--bits_per_sample",5 ) || !strcasecmp(argv[ i ],"-b") )                 
+                    format->PCM_bps = (short)atoi( argv[ ++i ] );
+
+            else if( !strncasecmp( argv[ i ],"--sample_rate",5 ) || !strcasecmp(argv[ i ],"-s") )                 
+                    format->SampleRate = (long)atoi( argv[ ++i ] );                    
+
+            else if( !strncasecmp( argv[ i ],"--usage",5 ) || !strcasecmp(argv[ i ],"-u") )                 
+                    usage(argv[0]);                    
+            else{
+                    fprintf(stderr,"%s: unknown argument on command line: %s\n",argv[0],argv[i]);
+                    usage(argv[0]);
+           }
+        }
+    }
 }
 
-void usage( char *binary_name )
+void usage(char *binary_name)
 {
-        
-        fprintf(stderr, "usage: %s -d [duration] -b [bps] \
-			\n-f [sampling freq] -u < instrument.dat > out.wav \n", binary_name);
-        
-        exit( EXIT_FAILURE );
-        
+        fprintf(stderr, "\nusage: %s [options] < input.txt > output.wav\n", binary_name); 
+        fprintf(stderr, "\nwhere [options] are:\n\n" );
+
+        fprintf(stderr, "--duration|-d arg\t\twhere arg is the duration in seconds [float]\n" );
+        fprintf(stderr, "--bits_per_sample|-b arg\twhere arg is the bits per sample of the wav file [int]\n" );
+        fprintf(stderr, "--sample_rate|-s arg\t\twhere arg is the sample rate of the wav file in Hertz [int]\n" );
+
+        fprintf(stderr, "\n\n");
+        exit( EXIT_FAILURE );        
 }
 
